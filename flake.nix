4bb39af506aedad144d9704508300f62a3df9567
techs-sus <92276908+techs-sus@users.noreply.github.com>
2026-08-20 12:28:19 -0400

    flake: use rustypipe-botguard at runtime

--- flake.nix
+++ flake.nix
@@ -25,6 +25,64 @@
 
         unfilteredRoot = ./.;
 
+        rustyV8Version = "149.2.0";
+        rustyV8Targets = {
+          "x86_64-linux" = "x86_64-unknown-linux-gnu";
+          "aarch64-linux" = "aarch64-unknown-linux-gnu";
+          "x86_64-darwin" = "x86_64-apple-darwin";
+          "aarch64-darwin" = "aarch64-apple-darwin";
+        };
+        rustyV8Target = rustyV8Targets.${system};
+        rustyV8 = pkgs.fetchurl {
+          url = "https://github.com/denoland/rusty_v8/releases/download/v${rustyV8Version}/librusty_v8_simdutf_release_${rustyV8Target}.a.gz";
+          sha256 =
+            {
+              "x86_64-linux" = "sha256-Uzfff0kIk7MAVpOZmFn7Pb24QPYsyTXJi/WcnL7kiVc=";
+              "aarch64-linux" = "sha256-eWUL59b9TQH111GE1ZaUS9SS7XD5ruBheojHT7klTiY=";
+              "x86_64-darwin" = "sha256-1yHm1WslC1CbXIjSxNI1mEJqqq7SRAVByO8BkS1Tk5s=";
+              "aarch64-darwin" = "sha256-W8fpKRWz6LgAB8Tu1501Ky8L2FOn4rGocDVHV3UN7Jk=";
+            }
+            .${system};
+        };
+
+        rustypipe-botguard = craneLib.buildPackage {
+          pname = "rustypipe-botguard";
+          version = "0.1.2";
+
+          src = builtins.fetchGit {
+            url = "https://codeberg.org/ThetaDev/rustypipe-botguard.git";
+            rev = "f235ebc1ae89d0fa2c103ca705ff1a312b9be673";
+            narHash = "sha256-5FtH5yfi9HSPQ7lnCu4KzQrIdBLLp1VvkuGgYrZt8Yw=";
+          };
+
+          strictDeps = true;
+
+          RUSTY_V8_ARCHIVE = rustyV8;
+
+          buildFeatures = [ ];
+
+          nativeBuildInputs = with pkgs; [
+            pkg-config
+            makeWrapper
+          ];
+
+          buildInputs = with pkgs; [
+            openssl
+            libclang
+          ];
+
+          # disable tests
+          doCheck = false;
+
+          meta = {
+            description = "Tool to run YouTube Botguard challenges and generate PO tokens";
+            homepage = "https://codeberg.org/ThetaDev/rustypipe-botguard";
+            license = lib.licenses.mit;
+            platforms = lib.platforms.unix;
+            mainProgram = "rustypipe-botguard";
+          };
+        };
+
         commonArgs = {
           pname = "music-syncer";
           version = "0.1.0";
@@ -53,8 +111,12 @@
 
           buildInputs = with pkgs; [
             # openssl
-            ffmpeg-headless
             libclang
+
+            # used to convert webm -> m4a
+            ffmpeg-headless
+
+            rustypipe-botguard
           ];
         };
 
@@ -94,6 +156,27 @@
 
         apps.default = flake-utils.lib.mkApp {
           drv = finalPackage;
+        };
+
+        apps.nix-update-rusty-v8 = {
+          type = "app";
+          program = toString (
+            pkgs.writeShellScript "update-rusty-v8" ''
+              set -euo pipefail
+
+              function update() {
+                sys=$1
+                target=$2
+                url="https://github.com/denoland/rusty_v8/releases/download/v${rustyV8Version}/librusty_v8_simdutf_release_$target.a.gz"
+                hash=$(nix store prefetch-file --hash-type sha256 --json "$url" | ${pkgs.lib.getExe pkgs.jq} -r '.hash')
+                sed -i "s|\"$sys\" = \"sha256-[^\"]*\";|\"$sys\" = \"$hash\";|" flake.nix
+              }
+
+              ${lib.concatStringsSep "\n" (
+                lib.mapAttrsToList (sys: target: "update ${sys} ${target}") rustyV8Targets
+              )}
+            ''
+          );
         };
 
         devShells.default = craneLib.devShell {
