ead4fc14eec92ef273d78d0516e6ccc58758fd44
techs-sus <92276908+techs-sus@users.noreply.github.com>
2026-09-07 16:00:56 -0400

    chore: add `./gradlew dependencyUpdates`
    - chore: update clikt to 5.1.0
    - chore: update kotlin to 2.4.20

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,5 @@
 plugins {
-	kotlin("jvm") version "2.4.10"
+	kotlin("jvm") version libs.versions.kotlin
 	alias(libs.plugins.sqldelight)
 	application
 }
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -20,4 +20,5 @@ dependencyResolutionManagement {
 
 plugins {
 	id("org.gradle.toolchains.foojay-resolver-convention") version "1.0.0"
+	id("io.github.ben-manes.versions.settings") version "0.61.0"
 }
